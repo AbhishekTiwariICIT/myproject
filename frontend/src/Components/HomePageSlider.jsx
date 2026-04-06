cbc5892af17777be60dd210d5150056a2ed24e15
Abhishek Tiwari <homedevendra@gmail.com>
2026-04-06 08:49:55 -0700

    image file name changed

--- frontend/src/Components/HomePageSlider.jsx
+++ frontend/src/Components/HomePageSlider.jsx
@@ -5,7 +5,7 @@ import Carousel from './Carousel';
 import SectionCard from '../Components/schResultsUpdate/SectionCard.jsx';
 import { Homepagemessage } from './Homepagmessage';
 import Bouncetemp from './Bounncetem';
-import pic1 from '../assets/Images/pic1.jpg';
+import PIC1 from '../assets/Images/PIC1.jpg';
 import pic2 from '../assets/Images/pic2.jpg';
 import pic3 from '../assets/Images/pic3.jpg';
 import pic4 from '../assets/Images/pic4.jpg';
@@ -121,7 +121,7 @@ export default function HomePageSlider() {
             )}
             <div className={styles.heroSection}>
                 <Carousel>
-                    <img src={pic1} alt="image 1 not found" />
+                    <img src={PIC1} alt="image 1 not found" />
                     <img src={pic2} alt="image 2 not found" />
                     <img src={pic3} alt="image 3 not found" />
                     <img src={pic4} alt="image 4 not found" />
